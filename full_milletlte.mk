a5e2cf99606fc9290d49aec53dc8c076c691365e
matteo0026 <marmotta200026@gmail.com>
2021-03-22 22:23:37 +0100

    milletlte: common_full_phone, various fixes
    common_full_phone = common_full_tablet_wifionly + telephony.mk, so it's the same thing

--- full_milletlte.mk
+++ full_milletlte.mk
@@ -15,7 +15,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from s3ve3g device
+# Inherit from milletlte device
 $(call inherit-product, device/samsung/milletlte/device.mk)
 
 # Set those variables here to overwrite the inherited values.
